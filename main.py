1871b95dc3af51f52cd5c7172482d0f3b6e5a12d
zyy <zyy15092467167@163.com>
2021-09-24 23:45:05 +0800

    提交main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 23:41:45 2021
+
+@author: admin
+"""
+def f(x,y):
+    print(x+y)
+def main():
+    f(1,2)
+    f(3,4)
+if __name__=='__main__':
+    main()
+    
